b77c1c0af8405e25ef2884372ef08b393a473f39
aaHung <landxh@gmail.com>
2014-03-05 00:14:30 +0800

    log instead of email notification

--- website/analytics.php
+++ website/analytics.php
@@ -11,13 +11,16 @@
 </body>
 </html>
 <?php
-if ($_POST['text'] && $_POST['text'] != "" && $_POST['text'] != "十九八七六五四三二一开始！欢迎使用“快读啦”，你可以不用移动你的眼球就能阅读，相信大家已经明白怎么使用了，请尽兴！"){
-	$str = "快读啦信息：来自";
+date_default_timezone_set('Asia/Taipei');
+if ($_POST['text'] && $_POST['text'] != "" && strpos($_POST['text'], "欢迎使用“快读啦") == false){
+	$str = date("F j, Y, g:i a") . "-快读啦信息：来自";
 	$str .= $_SERVER["REMOTE_ADDR"];
 	$str .= "的朋友读了：";
 	$str .= $_POST['text'];
-	error_log($str, 1, 'landxh@gmail.com');
-	error_log($str, 1, '496851081@qq.com');
+	$str .= "\n";
+	file_put_contents("nimda/log.txt", $str, FILE_APPEND);
+	// error_log($str, 1, 'landxh@gmail.com');
+	// error_log($str, 1, '496851081@qq.com');
 } else {
 
 }
