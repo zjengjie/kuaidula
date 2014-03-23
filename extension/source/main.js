d9ba5a0408beed9e086f49b49ed797a502f250ec
aaHung <landxh@gmail.com>
2014-03-23 15:27:48 +0800

    Optimize CSS

--- extension/source/main.js
+++ extension/source/main.js
@@ -10,14 +10,12 @@ chrome.runtime.onMessage.addListener(
 function ReadWindow(got){
         var eleContainer = document.createElement("div");
         this.eleContainer = eleContainer;
-        var position_left = $(window).width()/2;
-        var position_top = $(window).height()/2;
-        $(eleContainer).addClass("box-container").css('left',position_left+"px").css('top',position_top+"px"); 
-        $(eleContainer).css('box-shadow','10px 10px 5px #888');
+        $(eleContainer).addClass("box-container");
+        $(eleContainer).css('box-shadow','0px 0px 10px rgba(0, 0, 0, .5)');
         //from kuaidula.com index.php
         $(eleContainer).append("<div><button id='close'>X</button></div>");
         $(eleContainer).append("<div id='display' style='text-align: center;margin-top:90px'>快读啦</div>");
-        $(eleContainer).append("<textarea id='input' placeholder='请在此处输入想要阅读的文字' style='width: 100%; align-content:center; text-align:center' '></textarea>");
+        $(eleContainer).append("<div style='text-align: center'><textarea id='input' placeholder='请在此处输入想要阅读的文字' style='width: 80%;'></textarea></div>");
         $(eleContainer).append("<div style='text-align: center; margin-top: 3px;'><label>Speed:</label><input value='12' type='number' id='speed' style='width: 70px;'>字/秒</div>");
         $(eleContainer).append("<div class='row'><div style='align-content:center;text-align: center'><button id='go' style='width: 50%;text-align: center; align-content:center'>快读啦</button></div><div style='align-content:center;text-align: center'><button id='reset' style='width: 50%' >重置</button></div></div>");
         
